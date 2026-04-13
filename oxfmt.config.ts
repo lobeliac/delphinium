cccde6787c93f252200fbe947e8a3aae50f17fb6
lobelia <lobelia@nerine.cyou>
2026-04-13 19:37:27 +0100

    Added oxfmt and its configuration

---
+++ oxfmt.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "oxfmt";
+
+export default defineConfig({
+  arrowParens: "always",
+  bracketSameLine: false,
+  objectWrap: "preserve",
+  semi: true,
+  quoteProps: "as-needed",
+  trailingComma: "none",
+  singleAttributePerLine: false,
+  tabWidth: 2
+});
